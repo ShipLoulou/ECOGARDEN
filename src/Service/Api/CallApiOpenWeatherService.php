b810dfed050acf6f90a5ef5272c63c517c410bf0
Loulou <shiploulou.dev@gmail.com>
2025-02-05 12:51:00 +0100

    Mise en place de la mise en cache pour l'API meteo

--- src/Service/Api/CallApiOpenWeatherService.php
+++ src/Service/Api/CallApiOpenWeatherService.php
@@ -2,6 +2,8 @@
 
 namespace App\Service\Api;
 
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class CallApiOpenWeatherService
@@ -9,26 +11,32 @@ class CallApiOpenWeatherService
     private $APIkey = 'e2b2904deb1eff42eb3971f85d049123';
 
     public function __construct(
-        private HttpClientInterface $client
+        private HttpClientInterface $client,
+        private CacheInterface $cache
     ) {}
 
     public function getOpenWeatherApi($cityName): ?array
     {
-        $arrayData = $this->getGeocodingApi($cityName);
+        return $this->cache->get('api_data', function (ItemInterface $item, $cityName) {
+            // Définir la durée de vie du cache (ex: 1 heure)
+            $item->expiresAfter(3600);
 
-        if ($arrayData !== []) {
-            $lat = $arrayData[0]['lat'];
-            $lon = $arrayData[0]['lon'];
+            $arrayData = $this->getGeocodingApi($cityName);
 
-            $response = $this->client->request(
-                'GET',
-                'https://api.openweathermap.org/data/3.0/onecall?lat=' . $lat . '&lon=' . $lon . '&appid=' . $this->APIkey
-            );
+            if ($arrayData !== []) {
+                $lat = $arrayData[0]['lat'];
+                $lon = $arrayData[0]['lon'];
 
-            return $response->toArray();
-        }
+                $response = $this->client->request(
+                    'GET',
+                    'https://api.openweathermap.org/data/3.0/onecall?lat=' . $lat . '&lon=' . $lon . '&appid=' . $this->APIkey
+                );
 
-        return null;
+                return $response->toArray();
+            }
+
+            return null;
+        });
     }
 
     private function getGeocodingApi(string $cityName): array
